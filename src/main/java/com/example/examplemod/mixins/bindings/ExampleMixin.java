9c3637b5a4ef0ddb83a91b2915f821de7600cb8c
pleahmacaka <kkjuh0905@gmail.com>
2022-06-26 05:16:41 +0900

    :star: Mixin

---
+++ src/main/java/com/example/examplemod/mixins/bindings/ExampleMixin.java
@@ -0,0 +1,28 @@
+package com.example.examplemod.mixins.bindings;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.User;
+import net.minecraft.client.main.GameConfig;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.io.File;
+
+@Mixin(Minecraft.class)
+public class ExampleMixin {
+    @Shadow
+    @Final
+    private User user;
+
+    @Inject(at = @At("RETURN"), method = "<init>(Lnet/minecraft/client/main/GameConfig;)V", locals = LocalCapture.CAPTURE_FAILHARD)
+    private void init(GameConfig gameConfig, CallbackInfo callbackInfo, File file1) {
+        System.out.println("Mixin : Hello Forge Template!");
+        System.out.println("Mixin : Hello " + this.user.getName() + "!");
+        System.out.println("Mixin : Asset Dir - " + file1.getAbsolutePath());
+    }
+}
