96e01de46c000ad8274e6050b64c6d2766fb2cc3
PleahMaCaka <kkjuh0905@gmail.com>
2022-07-09 23:53:38 +0900

    :diamond_shape_with_a_dot_inside: Sad Obsidian Maker

---
+++ src/main/kotlin/common/items/SadObsidianMaker.kt
@@ -0,0 +1,27 @@
+package com.pleahmacaka.examplemod.common.items
+
+import com.pleahmacaka.examplemod.common.init.ExampleCreativeModTab
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.context.UseOnContext
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.Blocks
+
+
+object SadObsidianMaker : Item(
+    Properties()
+        .tab(ExampleCreativeModTab)
+        .stacksTo(1)
+) {
+    override fun useOn(context: UseOnContext): InteractionResult {
+        val level: Level = context.level
+        val blockPos = context.clickedPos
+
+        val selectedBlock = level.getBlockState(blockPos).block
+
+        if (selectedBlock == Blocks.OBSIDIAN && !level.isClientSide()) {
+            level.setBlock(blockPos, Blocks.CRYING_OBSIDIAN.defaultBlockState(), 1)
+        }
+        return InteractionResult.SUCCESS
+    }
+}
