34ccf864ddc3e3fb99ef280e1fc74a19db616642
pleahmacaka <pleahmacaka@gmail.com>
2023-01-13 03:29:47 +0900

    :star:

--- src/main/java/mixin/bindings/ExampleBindingMixin.java
+++ src/main/java/mixin/bindings/ExampleBindingMixin.java
@@ -2,11 +2,8 @@ package mixin.bindings;
 
 import com.pleahmacaka.examplemod.mixin.ExampleMixinBindingKt;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.User;
 import net.minecraft.client.main.GameConfig;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
