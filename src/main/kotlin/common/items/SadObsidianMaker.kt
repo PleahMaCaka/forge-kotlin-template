124ec72fd11188eb078e6b38c4b09c6022929b9d
pleahmacaka <pleahmacaka@gmail.com>
2023-03-23 01:09:14 +0900

    :star: better code

--- src/main/kotlin/common/items/SadObsidianMaker.kt
+++ src/main/kotlin/common/items/SadObsidianMaker.kt
@@ -6,7 +6,6 @@ import net.minecraft.sounds.SoundSource
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.context.UseOnContext
-import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.SoundType
 
@@ -16,13 +15,12 @@ object SadObsidianMaker : Item(
         .stacksTo(1)
 ) {
     override fun useOn(context: UseOnContext): InteractionResult {
-        val level: Level = context.level
+        if (!context.level.isClientSide) return InteractionResult.PASS
+
+        val level = context.level as ServerLevel
         val blockPos = context.clickedPos
 
-        val selectedBlock = level.getBlockState(blockPos).block
-
-        if (selectedBlock == Blocks.OBSIDIAN && !level.isClientSide()) {
-
+        if (level.getBlockState(blockPos).block == Blocks.OBSIDIAN) {
             level.setBlock(blockPos, Blocks.CRYING_OBSIDIAN.defaultBlockState(), 1)
             level.playSound(
                 null,
@@ -32,7 +30,7 @@ object SadObsidianMaker : Item(
                 1.0f,
                 1.0f
             )
-            (level as ServerLevel).sendParticles(
+            level.sendParticles(
                 ParticleTypes.LANDING_OBSIDIAN_TEAR,
                 blockPos.x.toDouble() + 0.5,
                 blockPos.y.toDouble() + 1,
